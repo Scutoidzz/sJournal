26a7ea36c5a399195b88cc593ae338ee741f0d5c
Sterling Duncan <techedreal@gmail.com>
2025-12-07 13:39:00 -0600

    new install script

--- internetrun.sh
+++ internetrun.sh
@@ -3,9 +3,9 @@ echo "The installation will start in 10 seconds. Press Ctrl+C to cancel."
 sleep 10
 echo "Downloading sJournal"
 git clone https://github.com/scutoid/sJournal.git
+cd sJournal
 echo "Installing pip dependencies"
 pip install -r requirements.txt --break-system-packages
-cd sJournal
 echo "Downloaded. Running app"
 echo "..."
 sleep 1
